73e9485074a8d2f359e8fb094618d0a7cc2c93ce
Gabit <gbzh2020gmail.com>
2022-02-27 21:51:30 +0600

    Brain prime

--- src/index.js
+++ src/index.js
@@ -7,4 +7,4 @@ export const name = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${name}!`);
 
 export const fun = (num, F, name) => console.log(`'${num}' is wrong answer ;(. Correct answer was '${F}'.\n Let's try again, ${name}!`);
-export const fun2 = (name) => console.log(`Congratulations, ${name}`);
+export const fun2 = (name) => console.log(`Congratulations, ${name}!`);
