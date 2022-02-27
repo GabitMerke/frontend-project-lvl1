828517d9c3cbe8f96d4ce74fa2c644caad642294
Gabit <gbzh2020gmail.com>
2022-02-27 21:37:50 +0600

    Brain prime

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -6,6 +6,14 @@ console.log('Welcome to the Brain Games!');
 const name = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${name}! \nAnswer "yes" if the number is even, otherwise answer "no".`);
 let randNum = Math.round(Math.random() * 100);
+const func4 = (arg) => {
+    if (arg % 2 === 0) {
+        return 'yes';
+    }
+    else {
+        return 'no';
+    }
+};
 
 for (let n = 0; n < 3; n += 1) {
     const num = readlineSync.question(`Question: ${randNum} \nYour answer: `);
@@ -18,7 +26,7 @@ if (randNum % 2 === 0 && num === 'yes') {
  
     randNum = Math.round(Math.random() * 100);
 } else {
-    console.log('\'yes\' is wrong answer ;(. Correct answer was \'no\'. \n Let\'s try again, Bill!');
+    console.log(`${num} is wrong answer ;(. Correct answer was ${func4(randNum)}. \n Let's try again, ${name}!`);
 break;
 }
 if (n === 2) {
