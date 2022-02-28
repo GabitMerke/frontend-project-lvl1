5c3d195f7807463c3273ee576416ba8ccd1f5125
Gabit <gbzh2020gmail.com>
2022-02-28 16:43:23 +0600

    Brain prime

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -6,10 +6,10 @@ import * as say from '../src/index.js';
 let randNum1 = Math.round(Math.random() * 10);
 let randNum2 = Math.round(Math.random() * 10);
 
-const NOD =(x, y) => {
-	if (y > x) return NOD(y, x);
-	if (!y) return x;
-	return NOD(y, x % y);
+const NOD = (x, y) => {
+if (y > x) return NOD(y, x);
+if (!y) return x;
+return NOD(y, x % y);
 };
 for (let n = 0; n < 3; n += 1) {
     const num = readlineSync.question(`Question: ${randNum1} ${randNum2} \nYour answer: `);
@@ -20,7 +20,8 @@ for (let n = 0; n < 3; n += 1) {
 } else {
     say.fun(num, NOD(randNum1, randNum2), say.name);
     break;
-} if (n == 2) {
+}
+if (n === 2) {
     say.fun2(say.name);
-  }   
+}   
 }
